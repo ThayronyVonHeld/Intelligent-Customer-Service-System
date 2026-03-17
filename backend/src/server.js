8985c457b704a2412ad428690e76c629fa65b2a8
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-03-17 15:07:44 -0300

    Integrando o mongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import dotenv from "dotenv";
+dotenv.config();
 
+import connectDB from "./config/db.js";
+
+connectDB();
 dotenv.config();
 
 const app = express();
