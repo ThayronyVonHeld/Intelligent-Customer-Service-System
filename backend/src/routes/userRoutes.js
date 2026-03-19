1c96e68508e0c7a41030eeb3ed88dce526085fbc
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-03-19 13:20:06 -0300

    Finish JWT

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,31 +1,76 @@
 import express from "express";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Criar usuário
 router.post("/", async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = new User({
       name,
       email,
-      password,
+      password: hashedPassword,
       role
     });
 
     await user.save();
 
     res.status(201).json(user);
-  } catch (error) {
-     if (error.code === 11000) {
-    return res.status(400).json({ error: "Email já cadastrado" });
-  }
 
-  res.status(500).json({ error: error.message });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ error: "Email já cadastrado" });
+    }
+
+    res.status(500).json({ error: error.message });
   }
-  
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: "Usuário não encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ error: "Senha inválida" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login realizado com sucesso",
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/profile", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 export default router;
